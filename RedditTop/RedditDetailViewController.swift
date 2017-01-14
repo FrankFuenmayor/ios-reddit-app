c94fd69a06b78aa42dc9adadbae99f9e3836e08b
Frank G. Fuenmayor G <frank.fuenmayor@gmail.com>
2017-01-14 18:32:08 -0400

    Add Action a RedditDetail

--- RedditTop/RedditDetailViewController.swift
+++ RedditTop/RedditDetailViewController.swift
@@ -11,18 +11,39 @@ import UIKit
 class RedditDetailViewController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var btnClose: UIBarButtonItem!
+    @IBOutlet weak var btnShare: UINavigationItem!
     
     var redditEntry : RedditEntry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        webView.loadRequest(URLRequest(url: redditEntry!.url!))    
+        webView.loadRequest(URLRequest(url: redditEntry!.url!))
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(close))
-
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cerrar",
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(close))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(shareAction))
     }
     
     func close() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func shareAction() {
+        let activities = [
+            UIActivityType.message,
+            UIActivityType.postToFacebook,
+            UIActivityType.postToTwitter,
+        ]
+        
+        let activityViewController = UIActivityViewController.init(activityItems: activities,
+                                                     applicationActivities: nil)
+        
+        present(activityViewController, animated: true, completion: nil)
     }
 }
